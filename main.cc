c5229f7a441b9d478a80f424321bc94b9cc26ae5
Masashi YOKOTA <yktmss@gmail.com>
2023-01-02 05:20:38 +0900

    my first ray tracing codes

--- main.cc
+++ main.cc
@@ -4,8 +4,21 @@
 
 #include <iostream>
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
-    // Liner interpolation between white and blue using y direction of the ray.
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+
+    // Liner interpolation between white and blue using y-direction of the ray.
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
