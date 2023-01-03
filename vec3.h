38369376d37e58f64da23939b6e23cc8867cfed0
Masashi YOKOTA <yktmss@gmail.com>
2023-01-04 06:01:59 +0900

    Lambertian

--- vec3.h
+++ vec3.h
@@ -114,4 +114,12 @@ vec3 ramdom_in_unit_sphere() {
         return p;
     }
 }
+
+vec3 random_unit_vector() {
+    auto a = random_double(0, 2*pi);
+    auto z = random_double(-1, 1);
+    auto r = sqrt(1 - z*z);
+    return vec3(r*cos(a), r*sin(a), z);
+}
+
 #endif
